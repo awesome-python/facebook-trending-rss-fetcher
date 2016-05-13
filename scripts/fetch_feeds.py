c284f28568f0f1ce7352ba92e31f030a0dc978f4
dantnguyen <dan@danwin.com>
2016-05-13 00:36:38 -0700

    whatever

--- scripts/fetch_feeds.py
+++ scripts/fetch_feeds.py
@@ -1,7 +1,9 @@
+from collections import OrderedDict
+from deco import *
+from datetime import datetime
 from pathlib import Path
 from re import sub
-from collections import OrderedDict
-from datetime import datetime
+from urllib.parse import urlparse
 import csv
 import json
 import requests
@@ -10,9 +12,8 @@ CSV_PATH = DATA_DIR.joinpath('rss-urls.csv')
 FEEDS_DIR = DATA_DIR.joinpath('feeds')
 FEEDS_DIR.mkdir(parents=True, exist_ok=True)
 
-
-for line in csv.DictReader(CSV_PATH.open('r')):
-    url = line['rss-url']
+@concurrent
+def fetch_feed(url):
     d = OrderedDict()
     d['requested_url'] = url
     d['fetched_at'] = datetime.now().isoformat()
@@ -29,7 +30,19 @@ for line in csv.DictReader(CSV_PATH.open('r')):
         d['headers'] = dict(resp.headers)
         d['response_url'] = resp.url
     finally:
-        fpath = FEEDS_DIR / (sub(r'[^\w\.\-]+', '_', url.split('//')[1]).strip('_') + '.json')
+        u = urlparse(url)
+        fn = u.netloc + '---' + u.path[1:] + '__' + u.query
+        fpath = FEEDS_DIR.joinpath(sub(r'[^\w\.\-]+', '_', fn.strip('_') + '.json'))
         with fpath.open('w') as f:
             json.dump(d, f, indent=2)
             print("Status code: {0}; Wrote to: {1}".format(d['status_code'], fpath))
+
+
+def main():
+    for line in csv.DictReader(CSV_PATH.open('r')):
+        url = line['rss-url']
+        fetch_feed(url)
+
+
+if __name__ == '__main__':
+    main()
